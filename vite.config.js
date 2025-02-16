691c0d34425050f8178d0bacc4f547fe6d51c7fb
Mavlonberdiyev Xudoyberdi <129485330+urgut200404@users.noreply.github.com>
2025-02-16 15:56:17 +0300

    Update vite.config.js
    asas

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,32 @@
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+  
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   css: {
+//     devSourcemap: true,
+//   },
+//   plugins: [vue()],
+// })
+
+
+
+
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-  
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  css: {
-    devSourcemap: true,
-  },
   plugins: [vue()],
+  build: {
+    rollupOptions: { 
+       output:{
+        manualChunks(id){
+          if(id.includes("node_modules")){
+            return id.toString().split('node_modules')[1].split('/').toString()
+          }
+        }
+      }
+    },
+  },
 })
